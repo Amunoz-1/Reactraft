e5c26167ff27a85ec15371415300e2df2f832035
Amir <amirm034@gmail.com>
2024-01-26 18:39:43 -0800

    Slides Toolbar Add-On staged

--- client/public/views/slidesScript.js
+++ client/public/views/slidesScript.js
@@ -6,3 +6,19 @@ function showSlide(number) {
   document.getElementById(`slide${number}`).style.display = 'block'; // Show the selected slide
   document.querySelector('.containerRight').style.visibility = 'visible'; // Make the right container visible
 }
+
+function changeSlide(slideNumber) {
+  document.querySelectorAll('.n2-bullet').forEach(function (bullet) {
+    bullet.classList.remove('n2-active');
+  });
+
+  document.querySelector(`#bullet${slideNumber}`).classList.add('n2-active');
+
+  const slideToScroll = document.getElementById(`slide${slideNumber}`);
+  if (slideToScroll) {
+    slideToScroll.scrollIntoView({
+      behavior: 'smooth',
+      block: 'center',
+    });
+  }
+}
